1b7c982e7bf84a8bfb8d766c12dca0ae3e2a98d6
eduardo-lamounier <eduardolamounierm@gmail.com>
2026-07-14 14:10:20 -0300

    Adiciona um script bash para rodar
    Adiciona um script bash (run.sh) para poder rodar o
    projeto pela linha de comando. Só funciona em Linux.
    Seria necessário fazer um script bat equivalente para
    fazer isso no Windows.

---
+++ run.sh
@@ -0,0 +1,4 @@
+javac -cp "src:$HOME/.m2/repository/com/github/micycle1/processing-core-4/4.5.0/processing-core-4-4.5.0.jar" -d bin src/Sketch.java
+
+java -cp "bin:$HOME/.m2/repository/com/github/micycle1/processing-core-4/4.5.0/processing-core-4-4.5.0.jar" Sketch
+
