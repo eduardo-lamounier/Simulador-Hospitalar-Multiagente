c51cfe37b4c2c01b1f6102c743e6c6e91c3fe952
eduardo-lamounier <eduardolamounierm@gmail.com>
2026-07-14 14:09:11 -0300

    Adiciona código base
    Adiciona um Sketch.java já com as principais funções
    do Processing.

---
+++ src/Sketch.java
@@ -0,0 +1,23 @@
+import processing.core.PApplet;
+
+public class Sketch extends PApplet {
+  public static void main(String[] args) {
+    PApplet.main("Sketch");
+  }
+
+  @Override
+  public void settings() {
+    
+  }
+
+  @Override
+  public void setup() {
+    
+  }
+
+  @Override
+  public void draw() {
+    
+  }
+}
+
