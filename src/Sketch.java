ad0631f8c21f39ebcd18c94397e530d068f4c270
Zerocentos <a2025950912@teiacoltec.org>
2026-07-21 17:08:12 -0300

    Implementa sistema de estados
    Implementa no arquivo Sketch o sistema de estados influenciando a tela
    que o programa mostra

--- src/Sketch.java
+++ src/Sketch.java
@@ -5,19 +5,37 @@ public class Sketch extends PApplet {
     PApplet.main("Sketch");
   }
 
+  private enum Estado {MENU, SIMULACAO, PAUSE};
+  private Estado estado_atual; 
+
   @Override
   public void settings() {
-    
+    size(800, 600);
   }
 
   @Override
   public void setup() {
-    
+    estado_atual = Estado.MENU;
   }
 
   @Override
   public void draw() {
+    background(255, 255, 255);
+
+    switch (estado_atual) {
+      case MENU:
+        
+        break;
     
+      case PAUSE:
+        break;
+
+      case SIMULACAO:
+        break;
+
+      default:
+        throw new IllegalStateException("Estado atual inválido!"); 
+    }
   }
 }
 
