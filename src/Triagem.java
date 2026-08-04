1435ff92203eb4dc6d6aec352c0561aead74a29a
eduardo-lamounier <eduardolamounierm@gmail.com>
2026-08-04 13:02:32 -0300

    Simplifica geração da árvore de decisão na triagem
    Define um método 'ehFolha()' que verifica se cor é ou não `null`, o que
    remove a necessidade de uma boolean `ehFolha` ser passada ao construtor
    da classe nó NoManchester.
    
    Um 'assert' no construtor verifica que pelo menos 'cor' OU 'decisao' são null,
    já que um nó folha não deve ter um predicado de decisão e um nó interno não
    deve ter uma cor.

--- src/Triagem.java
+++ src/Triagem.java
@@ -12,13 +12,15 @@ public class Triagem {
   }
 
   private class NoManchester {
-    public boolean ehFolha;
     public CorManchester cor;
     public Predicate<Paciente> decisao;
 
-    public NoManchester(boolean ehFolha, CorManchester cor,
-                         Predicate<Paciente> decisao) {
-      this.ehFolha = ehFolha;
+    public boolean ehFolha() { return cor != null; }
+
+    public NoManchester(CorManchester cor, Predicate<Paciente> decisao) {
+      assert cor != null && decisao == null
+             || cor == null && decisao != null;
+
       this.cor = cor;
       this.decisao = decisao;
     }
@@ -39,38 +41,33 @@ public class Triagem {
   private void gerarArvoreDecisao() {
     arvoreProtocoloManchester = new NoManchester[30+1];
 
-    arvoreProtocoloManchester[0] = new NoManchester(false, null,
+    arvoreProtocoloManchester[0] = new NoManchester(null,
       (var paciente) -> { return paciente.conscienciaAlterada(); }
     );
 
-    arvoreProtocoloManchester[1] = new NoManchester(true,
-      CorManchester.VERMELHO, null);
-    arvoreProtocoloManchester[2] = new NoManchester(false, null,
+    arvoreProtocoloManchester[1] = new NoManchester(CorManchester.VERMELHO, null);
+    arvoreProtocoloManchester[2] = new NoManchester(null,
       (var paciente) -> { return paciente.saturacaoOxigenio() < 92; }
     );
 
-    arvoreProtocoloManchester[5] = new NoManchester(true,
-      CorManchester.LARANJA, null);
-    arvoreProtocoloManchester[6] = new NoManchester(false, null,
+    arvoreProtocoloManchester[5] = new NoManchester(CorManchester.LARANJA, null);
+    arvoreProtocoloManchester[6] = new NoManchester(null,
       (var paciente) -> { return paciente.nivelDor() >= 8; }
     );
 
-    arvoreProtocoloManchester[13] = new NoManchester(true,
-      CorManchester.AMARELO, null);
-    arvoreProtocoloManchester[14] = new NoManchester(false, null,
+    arvoreProtocoloManchester[13] = new NoManchester(CorManchester.AMARELO, null);
+    arvoreProtocoloManchester[14] = new NoManchester(null,
       (var paciente) -> { return paciente.temperaturaCorporal() >= 38; }
     );
 
-    arvoreProtocoloManchester[29] = new NoManchester(true,
-      CorManchester.VERDE, null);
-    arvoreProtocoloManchester[30] = new NoManchester(true,
-      CorManchester.AZUL, null);
+    arvoreProtocoloManchester[29] = new NoManchester(CorManchester.VERDE, null);
+    arvoreProtocoloManchester[30] = new NoManchester(CorManchester.AZUL, null);
   }
 
   // Implementação recursiva da travesia pela árvore de decisão,
   // retornando - ao chegar em um nó folha - a cor do paciente
   private CorManchester corPaciente(int noAtual, Paciente paciente) {
-    if(arvoreProtocoloManchester[noAtual].ehFolha)
+    if(arvoreProtocoloManchester[noAtual].ehFolha())
       return arvoreProtocoloManchester[noAtual].cor;
 
     if(arvoreProtocoloManchester[noAtual].decisao.test(paciente))
