11f31059adbeeabb2e98aff4b8dac68e856f0c16
eduardo-lamounier <eduardolamounierm@gmail.com>
2026-08-11 18:07:07 -0300

    Declara função de leitura dos mapas
    A ideia de ler os arquivos dos mapas e retornar a matriz de
    caracteres estava um pouco misturada na função de imprimir
    as linhas dos arquivos. Esse commit define 'imprimirLinha' como
    um método 'void' que apenas imprime, enquanto um método 'lerMapa'
    (ainda não implementado) é declarado para retornar a matriz
    do grid do mapa.

--- src/LeitorDeMapa.java
+++ src/LeitorDeMapa.java
@@ -2,7 +2,13 @@ import java.io.*;
 
 public class LeitorDeMapa{
 
-public char[][] imprimirLinha(int seletMapa){
+    // Lê o arquivo do mapa especificado e retorna o grid desse mapa
+    // TODO: Implementar
+    public char[][] lerMapa(int seletMapa) {
+        throw new IllegalStateException("Essa função não foi ainda implementada");
+    }
+
+public void imprimirLinha(int seletMapa){
         try {
         String  NomeAqv = String.format("assets/Mapas/mapa%d.txt", seletMapa);
         //"Mapas/mapa" + seletMapa + ".txt"
@@ -20,9 +26,7 @@ public char[][] imprimirLinha(int seletMapa){
         br.close(); //Quando for necessário fechar
     } catch (IOException e) {
         e.printStackTrace();
-    }
-
-    throw new IllegalStateException("Essa função não foi ainda implementada");
+    } 
 } 
 
 }
