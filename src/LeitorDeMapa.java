4e53e80bfe98643bd890e00190ddbe1607381954
a2025954101-coder <a2025954101@teiacoltec.org>
2026-08-11 16:57:28 -0300

    wip: adiciona função de leitura do mapa

---
+++ src/LeitorDeMapa.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class LeitorDeMapa{
+
+public char[][] imprimirLinha(int seletMapa){
+        try {
+        String  NomeAqv = String.format("Mapas/mapa%d.txt", seletMapa);
+        //"Mapas/mapa" + seletMapa + ".txt"
+        /* 
+            LeitorDeMapa teste= new LeitorDeMapa();
+            int x = 3;
+            teste.imprimirLinha(x);
+        */
+        FileReader fileReader = new FileReader(NomeAqv);
+        BufferedReader br = new BufferedReader(fileReader);
+        String linha;
+        while ((linha = br.readLine()) != null) {
+            System.out.println(linha);
+        }
+        br.close(); //Quando for necessário fechar
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+
+    throw new IllegalStateException("Essa função não foi ainda implementada");
+} 
+
+}
