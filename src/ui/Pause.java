3f64811a557eff833686260f5e6d71fd9e337f21
Zerocentos <a2025950912@teiacoltec.org>
2026-08-31 15:16:40 -0300

    feat: Implementa atualiza e desenha da tela Pause
    Desenha a tela de pause com 4 botões diferentes, continuar na simulação,
    reiniciar a simulação, ir para a tela de seleção e sair do jogo.

--- src/ui/Pause.java
+++ src/ui/Pause.java
@@ -1,12 +1,73 @@
 package ui;
 
-public class Pause {
-    /*
-    ToDo -
+import processing.core.PApplet;
 
-    Botões: 
-        Reiniciar
-        Continuar
-        Voltar ao Menu
-    */
+public class Pause {
+/* ----------- Atributos ------------ */
+    // Sketch: 
+    PApplet p;
+    int width;
+    int height;
+
+    // Botões:
+    Botao continuarBT;
+    Botao reiniciarBT;
+    Botao sairBT;
+    Botao voltarSelecaoBT;
+
+/* ----------- Métodos ------------ */
+    // Construtor
+    public Pause(PApplet sketch) {
+        this.p = sketch;
+        this.width = sketch.width;
+        this.height = sketch.height;
+
+        continuarBT = new Botao(sketch, width/2, 7 * height/22, 2*width/3, height/11)
+                                .comArredondamento(20f)
+                                .comCor(0xFFFFFFFF)
+                                .comTexto("Continuar", 60, 0x00000000)
+                                .comAcao(null);
+
+        reiniciarBT = new Botao(sketch, width/2, 11 * height/22, 2*width/3, height/11)
+                                .comArredondamento(20f)
+                                .comCor(0xFFFFFFFF)
+                                .comTexto("Reiniciar", 60, 0x00000000)
+                                .comAcao(null);
+
+        voltarSelecaoBT = new Botao(sketch, width/2, 15 * height/22, 2*width/3, height/11)
+                                .comArredondamento(20f)
+                                .comCor(0xFFFFFFFF)
+                                .comTexto("Voltar para a seleção", 60, 0x00000000)
+                                .comAcao(null);
+
+        sairBT = new Botao(sketch, width/2, 19 * height/22, 2*width/3, height/11)
+                                .comArredondamento(20f)
+                                .comCor(0xFFFFFFFF)
+                                .comTexto("Sair", 60, 0x00000000)
+                                .comAcao(null);
+
+    }
+
+    public void atualiza() {
+        desenha();
+    }
+
+    public void desenha() {
+        fazerTitulo();
+
+        continuarBT.atualiza();
+        reiniciarBT.atualiza();
+        voltarSelecaoBT.atualiza();
+        sairBT.atualiza();
+    }
+
+    private void fazerTitulo() {
+        p.textAlign(PApplet.CENTER, PApplet.CENTER);
+        p.fill(0x00000000);
+
+        p.textSize(100);
+        p.text("Pausado", width/2, height/6);
+        
+              
+    }
 }
