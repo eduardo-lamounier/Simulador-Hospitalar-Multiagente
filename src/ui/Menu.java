1b4dfeeaf3e4572e950ed0e804e0ed4c7736690d
Zerocentos <a2025950912@teiacoltec.org>
2026-08-30 22:13:37 -0300

    Implementa temporização de clique nos botões
    A classe Menu é responsável por avisar os botões caso, haja um clique
    caso o clique for muito próximo em comparação com o clique anterior,
    no sentido de tempo, o menu ignora esse clique.

--- src/ui/Menu.java
+++ src/ui/Menu.java
@@ -19,23 +19,32 @@ public class Menu{
     // Botões:
     private Botao sair; // Botão para sair da simulação
     private Botao proxima_etapa; // Botão para ir a próxima etapa
+    private Botao ir_esquerda; // Botão para ir a esquerda na seleção de mapas
+    private Botao ir_direita; // Botoao para ir a direita na seleção de mapas
 
     // Menu:
     private int etapa = 1;
+
+    // Temporização:
+    private int cooldown = 200; // 200 milissegundos
+    private int clique_atual; // Em milissengundos
 
     public Menu(PApplet sketch) {
         this.p = sketch; // Mesma skecth da classe Skecth
         width = sketch.width;
         height = sketch.height;
 
-        //
+        // Atributos de temporização:
+        clique_atual = p.millis();
+
+        // Botões sendo montados:
         proxima_etapa = new Botao(sketch, width/2, 5 * height/12, 3 * width/5, height/  4)
                         .comArredondamento(20f)
                         .comCor(0xFFC4E1E6)
                         .comTexto("Iniciar", 100, 0xFF020202)
-                        .comAcao(() -> {etapa = 2;});
+                        .comAcao(() -> { etapa = 2; });
 
-        sair = new Botao(sketch, width/2, 9 * height/12, 3 * width/5, height/6)
+        sair = new Botao(sketch, width/2, 3 * height/4, 3 * width/5, height/6)
                         .comArredondamento(20f)
                         .comCor(0xFFC4E1E6)
                         .comTexto("Sair", 80, 0xFF000000)
@@ -43,15 +52,26 @@ public class Menu{
                                 System.out.println("Finalizando o programa...");
                                 sketch.exit();
                                 });
+
+        ir_esquerda = new Botao(sketch, width/10, 3 * height / 5, width/10, 6 * height / 10)
+                                .comCor(0xFFFFFFFF)
+                                .comAcao(() -> {});
     }
 
     public void desenha() {   
         fazerTitulo();
         
+        checaClique();
+        
         if(etapa == 1) {
             proxima_etapa.loop();
             sair.loop(); 
+
+            return;
         }
+
+        ir_esquerda.loop();
+
     }
 
     private void fazerTitulo() {
@@ -75,7 +95,19 @@ public class Menu{
                 break;
         }      
     }
+    
+    public void checaClique() {
+        if(p.millis() - clique_atual == cooldown) {
+            return;
+        }
 
+        clique_atual = p.millis();
+
+        proxima_etapa.clicado();
+        sair.clicado();
+        ir_esquerda.clicado();
+    }
+    
     public int getEtapa() {
         return etapa;
     }
